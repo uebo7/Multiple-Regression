05b67a64efec99ecbbc580e40079ec3588a2f22b
Adrian P Olweiler <apolweil@csciarch02.muad.local>
2026-04-20 17:28:54 -0400

    this is annoying

--- mult.cc
+++ mult.cc
@@ -9,6 +9,7 @@
  * Assumptions:
     - 3 sets of data
     - N is equal in every data set
+    - alpha is between .01 and .99
 
  * Notes:
     - substituting ta/2 for z score
@@ -18,62 +19,34 @@
  */
 
 /**************************************************************************/
-#include <algorithm>
+// include
+#include <boost/math/distributions/students_t.hpp>
+#include <cmath>
 #include <iostream>
 #include <print>
-#include <random>
 #include <span>
-// #include <thread>
-#include <condition_variable>
-#include <mutex>
-#include <type_traits>
+#include <thread>
 #include <vector>
+
+// local include
+
+#include "ThreadSafeQueue.hpp"
+#include "mult.hpp"
 
 /**************************************************************************/
 // headers
 
-unsigned
+struct Input
+{
+  unsigned n;
+  double alpha;
+};
+
+Input
 getInput ();
 
 void
 printResults ();
-
-template<typename T>
-T
-computeAverage (std::vector<T> dataValues);
-
-template<typename T>
-void
-findDataValues (std::vector<T>& dataValues, const T average);
-
-template<typename T, typename U>
-  requires std::is_arithmetic_v<T>
-void
-fillRandom (std::span<T> seq, U min, U max, unsigned seed);
-
-template<typename T>
-T
-calcSumOfSquares (std::vector<T> dataValues);
-
-template<typename T>
-T
-calcSumOfProducts (std::vector<T> firstValue, std::vector<T> secondValues);
-
-template<typename T>
-T
-calcSlope ();
-
-template<typename T>
-T
-computePointEstimate ();
-
-template<typename T>
-T
-computeStandardErr ();
-
-template<typename T>
-T
-findConfidenceInt ();
 
 /**************************************************************************/
 // main
@@ -81,11 +54,9 @@ findConfidenceInt ();
 int
 main ()
 {
-  unsigned N;
-  N = getInput ();
+  auto input = getInput ();
+  unsigned N = input.n;
   using type = float;
-  std::mutex m_mutex;
-  std::condition_variable m_cv;
 
   std::vector<type> x1 (N);
   std::vector<type> x2 (N);
@@ -94,107 +65,70 @@ main ()
   int min{1};
   int max{100};
   unsigned seed{1};
-
   fillRandom (std::span<type>{x1}, min, max, seed);
   fillRandom (std::span<type>{x2}, min, max, seed);
   fillRandom (std::span<type>{y}, min, max, seed);
 
   {
-    std::jthread averageX1 ([&] ()
-                            { findDataValues (x1, computeAverage (x1)); });
-    std::jthread averageX2 ([&] ()
-                            { findDataValues (x2, computeAverage (x2)); });
-    std::jthread averageY ([&] () { findDataValues (y, computeAverage (y)); });
-  }
+    std::jthread t1 (
+      [&]
+      {
+        auto avg1 = computeAverage (x1);
+        x1 = findDataValues (x1, avg1);
+      });
 
-  // std::println ("{}", data);
+    std::jthread t2 (
+      [&]
+      {
+        auto avg2 = computeAverage (x2);
+        x2 = findDataValues (x2, avg2);
+      });
+
+    std::jthread t3 (
+      [&]
+      {
+        auto avg3 = computeAverage (y);
+        y = findDataValues (y, avg3);
+      });
+  }
+  type sum1, sum2, sum3;
+  type prod1, prod2, prod3;
+  {
+    std::jthread sumsquare1 ([&] { sum1 = calcSumOfSquares (x1); });
+    std::jthread sumsquare2 ([&] { sum2 = calcSumOfSquares (x2); });
+    std::jthread sumsquare3 ([&] { sum3 = calcSumOfSquares (y); });
+
+    std::jthread sumproduct1 ([&] { prod1 = calcSumOfProducts (x1, x2); });
+    std::jthread sumproduct2 ([&] { prod2 = calcSumOfProducts (x2, y); });
+    std::jthread sumproduct3 ([&] { prod3 = calcSumOfProducts (x1, y); });
+  }
+  type (b1, b2) = calcSlopes (sum1, sum2, prod1, prod2, prod3);
+
+  std::println ("{}", x1);
+  std::println ("{}", x2);
+  std::println ("{}", y);
+
+  std::println ("{}", prod1);
+  std::println ("{}", prod2);
+  std::println ("{}", prod3);
 }
 
 /**************************************************************************/
 // functions
 
 // Needs N from user for amount of data obtained in each column
-unsigned
+// Needs alpha for confidence interval
+Input
 getInput ()
 {
+  Input in{};
   std::print ("Size   ==> ");
-  unsigned n;
-  std::cin >> n;
-  return n;
+  std::cin >> in.n;
+  std::print ("\nalpha  ==> ");
+  std::cin >> in.alpha;
+  return in;
 }
 
-/*
- * print the confidence interval
- */
+// print the confidence interval
 void
 printResults ();
-
-// fill each column with "N" random data values (range TBD)
-template<typename T, typename U>
-  requires std::is_arithmetic_v<T>
-void
-fillRandom (std::span<T> seq, U min, U max, unsigned seed)
-{
-  std::minstd_rand gen (seed);
-  if constexpr (std::is_floating_point_v<T>)
-  {
-    std::uniform_real_distribution<T> fDistribution (min, max);
-    std::ranges::generate (seq, [&] () { return fDistribution (gen); });
-  }
-  else
-  {
-    std::uniform_int_distribution<int> iDistribution (min, max);
-    std::ranges::generate (seq, [&] () { return iDistribution (gen); });
-  }
-}
-
-template<typename T>
-void
-findDataValues (std::vector<T>& dataValues, const T average)
-{
-  for (auto& x : dataValues)
-  {
-    x = x - average;
-  }
-}
-// uses jthreads to get average of all 3 columns of data in parallel
-template<typename T>
-T
-computeAverage (std::vector<T> dataValues)
-{
-  float total{};
-  for (float value : dataValues)
-    total += value;
-
-  float average = total / dataValues.size ();
-  return average;
-}
-
-// Use Jthreads to calculate sum of squares functions in parallel (3)
-// Requires averages from previous threads
-void
-calcSumOfSquares ();
-
-// Use Jthreads to calculate sum of products functions in parallel (3)
-void
-calcSumOfProducts ();
-
-// Use Jthreads to calculate slopes and intercept
-// relies on previous data
-
-void
-calcSlope ();
-
-// Calculate point estimate
-// No threads needed one operation and relies on previous input. Next step
-// relies on this
-void
-computePointEstimate ();
-
-// Use Jthreads to compute Standard Error of 3 random variables
-void
-computeStandardErr ();
-
-// Use Jthreads to find confidence interval of Beta
-void
-findConfidenceInt ();
